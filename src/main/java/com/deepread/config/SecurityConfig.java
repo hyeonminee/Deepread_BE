3e49a85e730376a44f4760d095d4697ccd2cf42d
hyunminee <phmin1110@naver.com>
2025-05-29 15:25:40 +0900

    security-swagger 수정

--- src/main/java/com/deepread/config/SecurityConfig.java
+++ src/main/java/com/deepread/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                                 "/api/auth/**",
                                 "/api/oauth/**",
                                 "/swagger-ui/**",
+                                "/swagger-ui.html",
                                 "/v3/api-docs/**"
                         ).permitAll()
                         .anyRequest().authenticated()
