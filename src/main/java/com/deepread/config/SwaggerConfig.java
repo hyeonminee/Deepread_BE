9f7ca10eb8e1f0a8141b1ca014dcae0dc3041803
hyunminee <phmin1110@naver.com>
2025-05-29 16:41:20 +0900

    swagger 설정

---
+++ src/main/java/com/deepread/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.deepread.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "DeepRead API",
+                version = "v1",
+                description = "DeepRead Backend API 문서"
+        ),
+        servers = @Server(
+                url = "/",
+                description = "Default Server URL"
+        )
+)
+public class SwaggerConfig {
+    // Swagger 설정은 annotation 기반이므로 별도 코드 불필요
+}
